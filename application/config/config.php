cb531918dd05897b0fdcd850e8bba49017c9bcd4
Mateus Silva Bastos <mtsbastos18@gmail.com>
2019-08-19 10:39:32 -0300

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rmv/';
-
+//$config['base_url'] = 'http://localhost/rmv/';
+$config['base_url'] = 'https://rivaversatilidade.herokuapp.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File
